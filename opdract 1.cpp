e5a8ba9b4de5591aea34e61bf90b7dc35eddae1e
Crystal <crystalsiao@gmail.com>
2024-12-04 23:33:15 -0300

    Add files via upload

---
+++ opdract 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number1;
+    cout << "enter number1: ";
+    cin >> number1;
+    int number2;
+    cout << "enter number2: ";
+    cin >> number2;
+    cout << number1 + number2;
+    return 0;
+}
